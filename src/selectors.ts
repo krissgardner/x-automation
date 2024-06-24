5d2ad4d5fd8ac1a69954dc27a0d6b378998bb93b
Kriss Gardner <krissrobert12@gmail.com>
2024-06-24 21:24:30 +0300

    Create selectors

---
+++ src/selectors.ts
@@ -0,0 +1,3 @@
+export const CONVERSATIONS =
+  "#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div.css-175oi2r.r-kemksi.r-16y2uox.r-f8sm7e.r-13qz1uu.r-1ye8kvj > section > div > div > div";
+export const CURRENT_MESSAGES = `#react-root > div > div > div > main > div > div > div > section:nth-child(2) > div > div > div > div > div > div > div > div > div[data-testid="DmScrollerContainer"] > div > div`;
