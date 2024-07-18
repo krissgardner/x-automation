593ba900e72acd205be83fd3095a582989bdfbf3
Kriss Gardner <krissrobert12@gmail.com>
2024-07-19 01:15:31 +0300

    Fix paths

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,12 @@
+import path from "path";
 import DBManager from "./DBManager";
 
-const dbManager = new DBManager("storage.json");
+const storagePath =
+  process.env.NODE_ENV === "production"
+    ? path.join(path.dirname(__dirname), "storage.json")
+    : "storage.json";
+
+const dbManager = new DBManager(storagePath);
 
 dbManager.checkIntegrity();
 
